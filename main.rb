6ae2c464cc146bac260d05b27daa32151ffa98ce
ut-kr <utkarsh@cyberfear.com>
2022-01-30 12:16:41 +1100

    ✨ main recurrance

--- main.rb
+++ main.rb
@@ -5,7 +5,17 @@ require './input_functions.rb'
 
 #?
 def main()
-
+  user_input = home_menu()
+  case user_input
+  when 1
+    # main function
+  when 5
+    # print goodbye
+  else
+      # ask to select for album
+      sleep(1)
+      main()
+  end
 end
 
 main()
